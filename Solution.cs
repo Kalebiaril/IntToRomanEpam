beff9417266e47db28aa04b11df3b87b144ad970
Galina Filippova <OSWGalinaF@oriflame.com>
2020-09-23 21:29:40 +0300

    removed not needed solution

--- Solution.cs
+++ Solution.cs
@@ -39,40 +39,5 @@ namespace IntToRomanLeetCode
             return builder.ToString();
         }
 
-
-        public string IntToRoman2(int num)
-        {
-            switch (num)
-            {
-                case int x when x >= 1000:
-                    return _romanNumeralsDictionary[1000] + IntToRoman(x - 1000);
-                case int x when x >= 900:
-                    return _romanNumeralsDictionary[900] + IntToRoman(x - 900);
-                case int x when x >= 500:
-                    return _romanNumeralsDictionary[500] + IntToRoman(x - 500);
-                case int x when x >= 400:
-                    return _romanNumeralsDictionary[400] + IntToRoman(x - 400);
-                case int x when x >= 100:
-                    return _romanNumeralsDictionary[100] + IntToRoman(x - 100);
-                case int x when x >= 90:
-                    return _romanNumeralsDictionary[90] + IntToRoman(x - 90);
-                case int x when x >= 50:
-                    return _romanNumeralsDictionary[50] + IntToRoman(x - 50);
-                case int x when x >= 40:
-                    return _romanNumeralsDictionary[40] + IntToRoman(x - 40);
-                case int x when x >= 10:
-                    return _romanNumeralsDictionary[10] + IntToRoman(x - 10);
-                case int x when x >= 9:
-                    return _romanNumeralsDictionary[9] + IntToRoman(x - 9);
-                case int x when x >= 5:
-                    return _romanNumeralsDictionary[5] + IntToRoman(x - 5);
-                case int x when x >= 4:
-                    return _romanNumeralsDictionary[4] + IntToRoman(x - 4);
-                case int x when x >= 1:
-                    return _romanNumeralsDictionary[1] + IntToRoman(x - 1);
-                default:
-                    return string.Empty;
-            }
-        }
     }
 }
